8e82ac7abcb539407fa65362fa429aebae142c4f
EvanErickson <evanserickson@gmail.com>
2020-09-11 20:57:54 -0700

    Initial Commit

---
+++ Simon Says/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simon Says
+//
+//  Created by Evan Erickson on 2020-09-11.
+//  Copyright © 2020 Evan Erickson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
